72cbfb59c28d4f7a256fc3c3f5f59105ef5530da
JmPotato <ghzpotato@gmail.com>
2021-06-21 11:03:56 +0800

    Make `GetTxnScopeFromConfig` return `TxnScope` from config directly (#93)
    Signed-off-by: JmPotato <ghzpotato@gmail.com>

--- config/config_test.go
+++ config/config_test.go
@@ -35,18 +35,10 @@ func (s *testConfigSuite) TestParsePath(c *C) {
 
 func (s *testConfigSuite) TestTxnScopeValue(c *C) {
 	c.Assert(failpoint.Enable("tikvclient/injectTxnScope", `return("bj")`), IsNil)
-	isGlobal, v := GetTxnScopeFromConfig()
-	c.Assert(isGlobal, IsFalse)
-	c.Assert(v, Equals, "bj")
-	c.Assert(failpoint.Disable("tikvclient/injectTxnScope"), IsNil)
+	c.Assert(GetTxnScopeFromConfig(), Equals, "bj")
 	c.Assert(failpoint.Enable("tikvclient/injectTxnScope", `return("")`), IsNil)
-	isGlobal, v = GetTxnScopeFromConfig()
-	c.Assert(isGlobal, IsTrue)
-	c.Assert(v, Equals, "global")
-	c.Assert(failpoint.Disable("tikvclient/injectTxnScope"), IsNil)
+	c.Assert(GetTxnScopeFromConfig(), Equals, "global")
 	c.Assert(failpoint.Enable("tikvclient/injectTxnScope", `return("global")`), IsNil)
-	isGlobal, v = GetTxnScopeFromConfig()
-	c.Assert(isGlobal, IsFalse)
-	c.Assert(v, Equals, "global")
+	c.Assert(GetTxnScopeFromConfig(), Equals, "global")
 	c.Assert(failpoint.Disable("tikvclient/injectTxnScope"), IsNil)
 }
