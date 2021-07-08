206b5dd027f2dc5a68370a5f4e29ae9f332ffae5
disksing <i@disksing.com>
2021-07-08 12:13:17 +0800

    move mocktikv to internal package (#209)
    Signed-off-by: disksing <i@disksing.com>
    
    Co-authored-by: Shirly <AndreMouche@126.com>

--- internal/mockstore/mocktikv/mvcc_leveldb.go
+++ internal/mockstore/mocktikv/mvcc_leveldb.go
@@ -47,7 +47,7 @@ import (
 	"github.com/pingcap/kvproto/pkg/kvrpcpb"
 	"github.com/pingcap/parser/terror"
 	"github.com/tikv/client-go/v2/internal/logutil"
-	"github.com/tikv/client-go/v2/mockstore/deadlock"
+	"github.com/tikv/client-go/v2/internal/mockstore/deadlock"
 	"github.com/tikv/client-go/v2/oracle"
 	"github.com/tikv/client-go/v2/util/codec"
 	"go.uber.org/zap"
