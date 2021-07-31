26a0968400258406facc5a3c07571a277b537edb
Shirly <AndreMouche@126.com>
2021-07-31 14:50:56 +0800

    tikv: move temporary export types to package txnkv (#258)
    Signed-off-by: shirly <AndreMouche@126.com>

--- integration_tests/go.mod
+++ integration_tests/go.mod
@@ -7,8 +7,8 @@ require (
 	github.com/pingcap/errors v0.11.5-0.20210425183316-da1aaba5fb63
 	github.com/pingcap/failpoint v0.0.0-20210316064728-7acb0f0a3dfd
 	github.com/pingcap/kvproto v0.0.0-20210722091755-91a52cd9e8db
-	github.com/pingcap/parser v0.0.0-20210707071004-31c87e37af5c
-	github.com/pingcap/tidb v1.1.0-beta.0.20210727065815-01ac21c194a5
+	github.com/pingcap/parser v0.0.0-20210728060616-75cff0c906d2
+	github.com/pingcap/tidb v1.1.0-beta.0.20210729073017-a27d306e65a0
 	github.com/stretchr/testify v1.7.0
 	github.com/tikv/client-go/v2 v2.0.0
 	github.com/tikv/pd v1.1.0-beta.0.20210323121136-78679e5e209d
---
+++ txnkv/util_export.go
@@ -0,0 +1,26 @@
+// Copyright 2021 TiKV Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package txnkv
+
+import "github.com/tikv/client-go/v2/txnkv/txnutil"
+
+// Priority is the priority for tikv to execute a command.
+type Priority = txnutil.Priority
+
+// Priority value for transaction priority.
+const (
+	PriorityHigh   = txnutil.PriorityHigh
+	PriorityNormal = txnutil.PriorityNormal
+	PriorityLow    = txnutil.PriorityLow
+)
