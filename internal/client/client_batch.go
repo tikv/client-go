c7fb3e7fc4784d87c9e8ed0c4add7860d189147e
tiancaiamao <tiancaiamao@gmail.com>
2021-08-19 15:55:45 +0800

    forget to include another modification
    Signed-off-by: tiancaiamao <tiancaiamao@gmail.com>

--- internal/client/client_batch.go
+++ internal/client/client_batch.go
@@ -8,7 +8,6 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -28,7 +27,6 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -795,6 +793,9 @@ func sendBatchRequest(
 }
 
 func (c *RPCClient) recycleIdleConnArray() {
+	c.recycleMu.Lock()
+	defer c.recycleMu.Unlock()
+
 	var addrs []string
 	c.RLock()
 	for _, conn := range c.conns {
