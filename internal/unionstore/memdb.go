843a5378aa9101c0e2aba61e0c2c11b3f122f08f
you06 <you1474600@gmail.com>
2021-10-09 12:30:03 +0800

    add readable flag (#320)
    Signed-off-by: you06 <you1474600@gmail.com>

--- internal/unionstore/memdb.go
+++ internal/unionstore/memdb.go
@@ -792,7 +792,7 @@ type memdbNode struct {
 	right memdbArenaAddr
 	vptr  memdbArenaAddr
 	klen  uint16
-	flags uint8
+	flags uint16
 }
 
 func (n *memdbNode) isRed() bool {
@@ -814,7 +814,7 @@ func (n *memdbNode) setBlack() {
 func (n *memdbNode) getKey() []byte {
 	var ret []byte
 	hdr := (*reflect.SliceHeader)(unsafe.Pointer(&ret))
-	hdr.Data = uintptr(unsafe.Pointer(&n.flags)) + 1
+	hdr.Data = uintptr(unsafe.Pointer(&n.flags)) + kv.FlagBytes
 	hdr.Len = int(n.klen)
 	hdr.Cap = int(n.klen)
 	return ret
@@ -822,8 +822,8 @@ func (n *memdbNode) getKey() []byte {
 
 const (
 	// bit 1 => red, bit 0 => black
-	nodeColorBit  uint8 = 0x80
-	nodeFlagsMask       = ^nodeColorBit
+	nodeColorBit  uint16 = 0x8000
+	nodeFlagsMask        = ^nodeColorBit
 )
 
 func (n *memdbNode) getKeyFlags() kv.KeyFlags {
@@ -831,5 +831,5 @@ func (n *memdbNode) getKeyFlags() kv.KeyFlags {
 }
 
 func (n *memdbNode) setKeyFlags(f kv.KeyFlags) {
-	n.flags = (^nodeFlagsMask & n.flags) | uint8(f)
+	n.flags = (^nodeFlagsMask & n.flags) | uint16(f)
 }
