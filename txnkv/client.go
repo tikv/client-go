6767326d20211bdf7da1b777442ff1334cc202cd
disksing <i@disksing.com>
2021-12-09 15:44:04 +0800

    move tikv.NewTxnClient to txnkv.NewClient
    Signed-off-by: disksing <i@disksing.com>

---
+++ txnkv/client.go
@@ -0,0 +1,57 @@
+// Copyright 2021 TiKV Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package txnkv
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/tikv/client-go/v2/config"
+	"github.com/tikv/client-go/v2/tikv"
+)
+
+// TxnKVClient is a txn client.
+type TxnKVClient struct {
+	*tikv.KVStore
+}
+
+// NewClient creates a txn client with pdAddrs.
+func NewClient(pdAddrs []string) (*TxnKVClient, error) {
+	cfg := config.GetGlobalConfig()
+	pdClient, err := tikv.NewPDClient(pdAddrs)
+	if err != nil {
+		return nil, err
+	}
+	// init uuid
+	uuid := fmt.Sprintf("tikv-%v", pdClient.GetClusterID(context.TODO()))
+	tlsConfig, err := cfg.Security.ToTLSConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	spkv, err := tikv.NewEtcdSafePointKV(pdAddrs, tlsConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := tikv.NewKVStore(uuid, pdClient, spkv, tikv.NewRPCClient(tikv.WithSecurity(cfg.Security)))
+	if err != nil {
+		return nil, err
+	}
+	if cfg.TxnLocalLatches.Enabled {
+		s.EnableTxnLocalLatches(cfg.TxnLocalLatches.Capacity)
+	}
+	return &TxnKVClient{KVStore: s}, nil
+}
