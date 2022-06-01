02e8fde261b0f41af19852989b6f6cd20d9eff50
ekexium <ekexium@gmail.com>
2022-06-01 10:31:11 +0800

    set is_retry_request
    Signed-off-by: ekexium <ekexium@gmail.com>

--- txnkv/transaction/prewrite.go
+++ txnkv/transaction/prewrite.go
@@ -213,6 +213,10 @@ func (action actionPrewrite) handleSingleBatch(c *twoPhaseCommitter, bo *retry.B
 	}()
 	for {
 		attempts++
+		if attempts > 1 || action.retry {
+			req.IsRetryRequest = true
+		}
+
 		if time.Since(tBegin) > slowRequestThreshold {
 			logutil.BgLogger().Warn("slow prewrite request", zap.Uint64("startTS", c.startTS), zap.Stringer("region", &batch.region), zap.Int("attempts", attempts))
 			tBegin = time.Now()
