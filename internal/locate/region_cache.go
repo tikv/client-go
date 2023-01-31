55a6bbbf23550c20a9de43bcf445b492a17293ca
Yilin Chen <sticnarf@gmail.com>
2023-01-31 16:15:24 +0800

    Rewind after cacheGC reaches the end (#678) (#679)
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>

--- internal/locate/region_cache.go
+++ internal/locate/region_cache.go
@@ -1645,7 +1645,8 @@ func (c *RegionCache) cacheGC() {
 	ticker := time.NewTicker(cleanCacheInterval)
 	defer ticker.Stop()
 
-	iterItem := newBtreeSearchItem([]byte(""))
+	beginning := newBtreeSearchItem([]byte(""))
+	iterItem := beginning
 	expired := make([]*btreeItem, cleanRegionNumPerRound)
 	for {
 		select {
@@ -1671,6 +1672,11 @@ func (c *RegionCache) cacheGC() {
 				return true
 			})
 			c.mu.RUnlock()
+
+			// Reach the end of the region cache, start from the beginning
+			if count <= cleanRegionNumPerRound {
+				iterItem = beginning
+			}
 
 			if len(expired) > 0 {
 				c.mu.Lock()
