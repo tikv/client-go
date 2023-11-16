024b02a9a7d0bf1e32ba588979b817af75804caf
glorv <glorvs@163.com>
2023-11-16 12:03:11 +0800

    fix typo
    Signed-off-by: glorv <glorvs@163.com>

--- internal/client/client_interceptor.go
+++ internal/client/client_interceptor.go
@@ -123,7 +123,7 @@ func buildResourceControlInterceptor(
 			req.GetResourceControlContext().Penalty = penalty
 			// override request priority with resource group priority if it's not set.
 			// Get the priority at tikv side has some performance issue, so we pass it
-			// at client side. See: https://github.com/tikv/tikv/issues/15994 for more detials.
+			// at client side. See: https://github.com/tikv/tikv/issues/15994 for more details.
 			if req.GetResourceControlContext().OverridePriority == 0 {
 				req.GetResourceControlContext().OverridePriority = uint64(priority)
 			}
