3480b5ed7ce131ed39f2e6ffcdec603f4166e225
MyonKeminta <9948422+MyonKeminta@users.noreply.github.com>
2024-01-23 13:54:05 +0800

    Revert "fix: check kill signal against 0 (#1102)" (#1129)
    This reverts commit 057c479dd8c27ab6b2b4175321166a5d5228d750.
    
    Signed-off-by: MyonKeminta <MyonKeminta@users.noreply.github.com>
    Co-authored-by: MyonKeminta <MyonKeminta@users.noreply.github.com>

--- config/retry/backoff.go
+++ config/retry/backoff.go
@@ -217,9 +217,10 @@ func (b *Backoffer) BackoffWithCfgAndMaxSleep(cfg *Config, maxSleepMs int, err e
 		atomic.AddInt64(&detail.BackoffCount, 1)
 	}
 
-	err2 := b.CheckKilled()
-	if err2 != nil {
-		return err2
+	if b.vars != nil && b.vars.Killed != nil {
+		if atomic.LoadUint32(b.vars.Killed) == 1 {
+			return errors.WithStack(tikverr.ErrQueryInterrupted)
+		}
 	}
 
 	var startTs interface{}
@@ -380,18 +381,4 @@ func (b *Backoffer) longestSleepCfg() (*Config, int) {
 		}
 	}
 	return nil, 0
-}
-
-func (b *Backoffer) CheckKilled() error {
-	if b.vars != nil && b.vars.Killed != nil {
-		killed := atomic.LoadUint32(b.vars.Killed)
-		if killed != 0 {
-			logutil.BgLogger().Info(
-				"backoff stops because a killed signal is received",
-				zap.Uint32("signal", killed),
-			)
-			return errors.WithStack(tikverr.ErrQueryInterrupted)
-		}
-	}
-	return nil
 }
