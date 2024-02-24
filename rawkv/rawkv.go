4135ec7d30f3827626ea06f183168af3508c69fe
Denis Subbotin <denis@subbot.in>
2024-02-24 12:31:20 +0300

    check errors in scan request

--- rawkv/rawkv.go
+++ rawkv/rawkv.go
@@ -539,6 +539,9 @@ func (c *Client) Scan(ctx context.Context, startKey, endKey []byte, limit int, o
 		}
 		cmdResp := resp.Resp.(*kvrpcpb.RawScanResponse)
 		for _, pair := range cmdResp.Kvs {
+			if pair.Error != nil {
+				return nil, nil, errors.New(pair.Error.String())
+			}
 			keys = append(keys, pair.Key)
 			values = append(values, convertNilToEmptySlice(pair.Value))
 		}
@@ -588,6 +591,9 @@ func (c *Client) ReverseScan(ctx context.Context, startKey, endKey []byte, limit
 		}
 		cmdResp := resp.Resp.(*kvrpcpb.RawScanResponse)
 		for _, pair := range cmdResp.Kvs {
+			if pair.Error != nil {
+				return nil, nil, errors.New(pair.Error.String())
+			}
 			keys = append(keys, pair.Key)
 			values = append(values, convertNilToEmptySlice(pair.Value))
 		}
