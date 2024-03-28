348afcb31bc3f0015274e5ef4a66905ef4849842
y_static_y@sina.com <y_static_y@sina.com>
2024-03-28 10:53:25 +0800

    code format
    Signed-off-by: y_static_y@sina.com <y_static_y@sina.com>

--- oracle/oracle.go
+++ oracle/oracle.go
@@ -59,7 +59,7 @@ type Oracle interface {
 	GetExternalTimestamp(ctx context.Context) (uint64, error)
 	SetExternalTimestamp(ctx context.Context, ts uint64) error
 
-	GetMinTimestamp(ctx context.Context) (uint64, error)
+	GetMinTimestampInAllTSOGroup(ctx context.Context) (uint64, error)
 }
 
 // Future is a future which promises to return a timestamp.
