5a4905d2f5534dcf4238e1bdb19cde47775e6081
crazycs <crazycs520@gmail.com>
2024-04-03 13:22:40 +0800

    refine timedetail string (#1261)
    * refine timedetail string
    
    Signed-off-by: crazycs520 <crazycs520@gmail.com>
    
    * add test
    
    Signed-off-by: crazycs520 <crazycs520@gmail.com>
    
    * refine test
    
    Signed-off-by: crazycs520 <crazycs520@gmail.com>
    
    ---------
    
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- util/execdetails.go
+++ util/execdetails.go
@@ -635,7 +635,6 @@ func (td *TimeDetail) String() string {
 		return ""
 	}
 	buf := bytes.NewBuffer(make([]byte, 0, 16))
-	buf.WriteString("time_detail: {")
 	if td.ProcessTime > 0 {
 		buf.WriteString("total_process_time: ")
 		buf.WriteString(FormatDuration(td.ProcessTime))
@@ -668,8 +667,10 @@ func (td *TimeDetail) String() string {
 		buf.WriteString("tikv_wall_time: ")
 		buf.WriteString(FormatDuration(td.TotalRPCWallTime))
 	}
-	buf.WriteString("}")
-	return buf.String()
+	if buf.Len() == 0 {
+		return ""
+	}
+	return "time_detail: {" + buf.String() + "}"
 }
 
 // Merge merges the time detail into itself.
