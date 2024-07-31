9909bb87a67649ae25e76a94add2e0e843d1be62
crazycs520 <crazycs520@gmail.com>
2024-07-31 13:00:17 +0800

    fix
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- internal/locate/region_cache_test.go
+++ internal/locate/region_cache_test.go
@@ -2863,13 +2863,14 @@ func (s *testRegionCacheSuite) TestIssue1401() {
 		return s.getResolveState() == needCheck
 	})
 
-	// assert that the old store should be deleted and it's reachable
+	// assert that the old store should be deleted.
 	s.Eventually(func() bool {
-		return store1.getResolveState() == deleted && store1.getLivenessState() == reachable
+		return store1.getResolveState() == deleted
 	}, 3*time.Second, time.Second)
 	// assert the new store should be added and it should be resolved and reachable.
 	newStore1, _ := s.cache.stores.get(s.store1)
-	s.Require().Equal(resolved, newStore1.getResolveState())
-	s.Require().Equal(reachable, newStore1.getLivenessState())
+	s.Eventually(func() bool {
+		return newStore1.getResolveState() == resolved && newStore1.getLivenessState() == reachable
+	}, 3*time.Second, time.Second)
 	s.Require().True(isStoreContainLabel(newStore1.labels, "host", "0.0.0.0:20161"))
 }
