d3a5edf7c85e55b8b2085eeaa5aa321f45d19711
mittalrishabh <mittalrishabh@gmail.com>
2024-08-31 22:08:18 -0700

    Update region_request.go

--- internal/locate/region_request.go
+++ internal/locate/region_request.go
@@ -526,10 +526,6 @@ func (state *tryFollower) next(bo *retry.Backoffer, selector *replicaSelector) (
 	}
 
 	rpcCtx.contextPatcher.staleRead = &state.isStaleRead
-	if state.isStaleRead {
-		// reset the state so that next replica read is not stale
-		state.isStaleRead = false
-	}
 	return rpcCtx, nil
 }
 
