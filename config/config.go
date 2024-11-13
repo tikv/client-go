8de9ca0589f3a93a7391f813a6e9a7b6efdd8743
ekexium <eke@fastmail.com>
2024-11-13 15:55:10 +0800

    feat: allow setting resolved lock concurrency for pipelined dml
    Signed-off-by: ekexium <eke@fastmail.com>

--- config/config.go
+++ config/config.go
@@ -48,18 +48,23 @@ import (
 )
 
 var (
-	globalConf                atomic.Value
-	PipelinedFlushConcurrency atomic.Uint32
+	globalConf                  atomic.Value
+	PipelinedFlushConcurrency   atomic.Uint32
+	PipelinedResolveConcurrency atomic.Uint32
 )
 
 const (
 	// DefStoresRefreshInterval is the default value of StoresRefreshInterval
-	DefStoresRefreshInterval = 60
+	DefStoresRefreshInterval       = 60
+	DefPipelinedFlushConcurrency   = 128
+	DefPipelinedResolveConcurrency = 8
 )
 
 func init() {
 	conf := DefaultConfig()
 	StoreGlobalConfig(&conf)
+	PipelinedFlushConcurrency.Store(DefPipelinedFlushConcurrency)
+	PipelinedResolveConcurrency.Store(DefPipelinedResolveConcurrency)
 }
 
 // Config contains configuration options.
