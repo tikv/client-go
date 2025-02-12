af06f7b15d15f06f92bd2091099aed584a1f717a
ekexium <eke@fastmail.com>
2025-02-12 14:53:58 +0800

    (tidb-6.5) Do not set txn scope in snapshot (#1572)
    ref pingcap/tidb#59402
    
    Signed-off-by: ekexium <eke@fastmail.com>

--- txnkv/txnsnapshot/snapshot.go
+++ txnkv/txnsnapshot/snapshot.go
@@ -396,7 +396,6 @@ func (s *KVSnapshot) batchGetSingleRegion(bo *retry.Backoffer, batch batchKeys, 
 		matchStoreLabels := s.mu.matchStoreLabels
 		replicaAdjuster := s.mu.replicaReadAdjuster
 		s.mu.RUnlock()
-		req.TxnScope = scope
 		req.ReadReplicaScope = scope
 		if isStaleness {
 			req.EnableStaleRead()
@@ -604,7 +603,6 @@ func (s *KVSnapshot) get(ctx context.Context, bo *retry.Backoffer, k []byte) ([]
 	scope := s.mu.readReplicaScope
 	replicaAdjuster := s.mu.replicaReadAdjuster
 	s.mu.RUnlock()
-	req.TxnScope = scope
 	req.ReadReplicaScope = scope
 	var ops []locate.StoreSelectorOption
 	if isStaleness {
