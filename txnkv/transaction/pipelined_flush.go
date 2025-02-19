4003612bcfbfe686dd949d28c1cfcf0d235039fd
you06 <you1474600@gmail.com>
2025-02-19 10:46:31 +0900

    p-dml: resolve locks concurrently (#1584)
    close tikv/client-go#1577
    
    Signed-off-by: you06 <you1474600@gmail.com>

--- txnkv/transaction/pipelined_flush.go
+++ txnkv/transaction/pipelined_flush.go
@@ -485,6 +485,7 @@ func (c *twoPhaseCommitter) resolveFlushedLocks(bo *retry.Backoffer, start, end 
 		handler,
 	)
 	runner.SetStatLogInterval(30 * time.Second)
+	runner.SetRegionsPerTask(1)
 
 	c.txn.spawnWithStorePool(func() {
 		if err = runner.RunOnRange(bo.GetCtx(), start, end); err != nil {
