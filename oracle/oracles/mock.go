8f4f76934362588f28628e46290455755d2b3d8f
ekexium <eke@fastmail.com>
2025-02-25 19:37:19 +0800

    Validate ts only for stale read (#1592)
    ref pingcap/tidb#59402
    
    Signed-off-by: ekexium <eke@fastmail.com>

--- oracle/oracles/mock.go
+++ oracle/oracles/mock.go
@@ -36,14 +36,11 @@ package oracles
 
 import (
 	"context"
-	"math"
 	"sync"
 	"time"
 
 	"github.com/pkg/errors"
-	"github.com/tikv/client-go/v2/internal/logutil"
 	"github.com/tikv/client-go/v2/oracle"
-	"go.uber.org/zap"
 )
 
 var errStopped = errors.New("stopped")
@@ -127,21 +124,6 @@ func (o *MockOracle) GetLowResolutionTimestampAsync(ctx context.Context, opt *or
 
 // ValidateReadTS implements ReadTSValidator interface.
 func (o *MockOracle) ValidateReadTS(ctx context.Context, readTS uint64, isStaleRead bool, opt *oracle.Option) error {
-	if readTS == math.MaxUint64 {
-		if isStaleRead {
-			logutil.Logger(ctx).Error("stale read use MaxUint64 as readTS")
-		}
-		return nil
-	}
-
-	currentTS, err := o.GetTimestamp(ctx, opt)
-	if err != nil {
-		logutil.Logger(ctx).Error("fail to get timestamp when validating", zap.Error(err))
-		return nil
-	}
-	if currentTS < readTS {
-		logutil.Logger(ctx).Error("ts validation failed", zap.Uint64("readTS", readTS), zap.Uint64("currentTS", currentTS))
-	}
 	return nil
 }
 
