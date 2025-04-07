4f07f05b9d8d38a68507be63cf1e8bc4fa429a10
sujuntao <juntao.su@foxmail.com>
2025-04-07 23:13:51 +0800

    Use tikverr.IsErrWriteConflict(err1), not tikverr.IsErrWriteConflict(errors.Cause(err1))
    Signed-off-by: sujuntao <juntao.su@foxmail.com>

--- txnkv/transaction/pipelined_flush.go
+++ txnkv/transaction/pipelined_flush.go
@@ -227,7 +227,7 @@ func (action actionPipelinedFlush) handleSingleBatch(
 			// Extract lock from key error
 			lock, err1 := txnlock.ExtractLockFromKeyErr(keyErr)
 			if err1 != nil {
-				if tikverr.IsErrWriteConflict(errors.Cause(err1)) {
+				if tikverr.IsErrWriteConflict(err1) {
 					metrics.TiKVTxnWriteConflictCounter.Inc()
 				}
 				return err1
