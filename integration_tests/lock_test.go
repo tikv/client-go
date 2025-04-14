cd582a68df03bfeb3f44ad9a505cfc4e4c2359a0
cfzjywxk <cfzjywxk@gmail.com>
2025-04-14 16:31:56 +0800

    avoid zero ttl usage in test cases
    Signed-off-by: cfzjywxk <cfzjywxk@gmail.com>

--- integration_tests/lock_test.go
+++ integration_tests/lock_test.go
@@ -613,7 +613,7 @@ func (s *testLockSuite) prepareTxnFallenBackFromAsyncCommit() {
 	committer, err := txn.NewCommitter(1)
 	s.Nil(err)
 	s.Equal(committer.GetMutations().Len(), 2)
-	committer.SetLockTTL(0)
+	committer.SetLockTTL(1)
 	committer.SetUseAsyncCommit()
 	committer.SetCommitTS(committer.GetStartTS() + (100 << 18)) // 100ms
 
@@ -642,10 +642,16 @@ func (s *testLockSuite) TestCheckLocksFallenBackFromAsyncCommit() {
 	err = lr.CheckAllSecondaries(bo, lock, &status)
 	s.True(lr.IsNonAsyncCommitLock(err))
 
-	status, err = lr.GetTxnStatusFromLock(bo, lock, 0, true)
-	s.Nil(err)
+	for {
+		status, err = lr.GetTxnStatusFromLock(bo, lock, 0, true)
+		s.Nil(err)
+		if status.Action() == kvrpcpb.Action_TTLExpireRollback {
+			break
+		}
+	}
 	s.Equal(status.Action(), kvrpcpb.Action_TTLExpireRollback)
 	s.Equal(status.TTL(), uint64(0))
+	s.Equal(status.IsRolledBack(), true)
 }
 
 func (s *testLockSuite) TestResolveTxnFallenBackFromAsyncCommit() {
@@ -938,7 +944,7 @@ func (s *testLockSuite) TestResolveLocksForRead() {
 	locks = append(locks, lock)
 
 	// can't be pushed but is expired
-	startTS, _ = s.lockKey([]byte("k5"), []byte("v5"), []byte("k55"), []byte("v55"), 0, false, true)
+	startTS, _ = s.lockKey([]byte("k5"), []byte("v5"), []byte("k55"), []byte("v55"), 1, false, true)
 	committedLocks = append(committedLocks, startTS)
 	lock = s.mustGetLock([]byte("k5"))
 	locks = append(locks, lock)
@@ -972,6 +978,9 @@ func (s *testLockSuite) TestResolveLocksForRead() {
 	bo := tikv.NewBackoffer(context.Background(), getMaxBackoff)
 	lr := s.store.NewLockResolver()
 	defer lr.Close()
+
+	// Sleep for a while to make sure the async commit lock "k5" expires, so it could be resolve commit.
+	time.Sleep(100 * time.Millisecond)
 	msBeforeExpired, resolved, committed, err := lr.ResolveLocksForRead(bo, readStartTS, locks, false)
 	s.Nil(err)
 	s.Greater(msBeforeExpired, int64(0))
