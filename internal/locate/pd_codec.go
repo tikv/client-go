f6497dd93340ab628748e53d61c5566b0af3b044
ystaticy <y_static_y@sina.com>
2025-06-18 13:29:17 +0800

    fix lint
    Signed-off-by: ystaticy <y_static_y@sina.com>

--- internal/locate/pd_codec.go
+++ internal/locate/pd_codec.go
@@ -141,7 +141,6 @@ func (c *CodecPDClient) GetRegionByID(ctx context.Context, regionID uint64, opts
 // returned StartKey && EndKey from pd-server.
 func (c *CodecPDClient) ScanRegions(ctx context.Context, startKey []byte, endKey []byte, limit int, opts ...opt.GetRegionOption) ([]*router.Region, error) {
 	startKey, endKey = c.codec.EncodeRegionRange(startKey, endKey)
-	//nolint:staticcheck
 	regions, err := c.Client.ScanRegions(ctx, startKey, endKey, limit, opts...)
 	if err != nil {
 		return nil, errors.WithStack(err)
