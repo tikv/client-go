324a104d54be8a2d786bd99983ec1df60d11df02
xzhangxian1008 <xzhangxian@foxmail.com>
2025-08-05 10:00:03 +0800

    revert invalidate valid region

--- internal/locate/region_cache.go
+++ internal/locate/region_cache.go
@@ -1843,7 +1843,7 @@ func (c *RegionCache) BatchLoadRegionsWithKeyRange(bo *retry.Backoffer, startKey
 	// TODO(youjiali1995): scanRegions always fetch regions from PD and these regions don't contain buckets information
 	// for less traffic, so newly inserted regions in region cache don't have buckets information. We should improve it.
 	for _, region := range regions {
-		c.insertRegionToCache(region, false, false)
+		c.insertRegionToCache(region, true, false)
 		vid := region.VerID()
 		debugInfo = fmt.Sprintf("%s <region: %d, ver: %d, conf-ver: %d>", debugInfo, vid.GetID(), vid.GetVer(), vid.GetConfVer())
 	}
@@ -1872,7 +1872,7 @@ func (c *RegionCache) BatchLoadRegionsWithKeyRanges(bo *retry.Backoffer, keyRang
 
 	debugInfo := "tcmsdebug for regions: "
 	for _, region := range regions {
-		c.insertRegionToCache(region, false, false)
+		c.insertRegionToCache(region, true, false)
 		vid := region.VerID()
 		debugInfo = fmt.Sprintf("%s <region: %d, ver: %d, conf-ver: %d>", debugInfo, vid.GetID(), vid.GetVer(), vid.GetConfVer())
 	}
