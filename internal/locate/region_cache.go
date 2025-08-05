8f943454a4104a0e02536bffb1b3b04ebf84d74a
xzhangxian1008 <xzhangxian@foxmail.com>
2025-08-05 09:02:40 +0800

    tweaking

--- internal/locate/region_cache.go
+++ internal/locate/region_cache.go
@@ -1066,20 +1066,6 @@ func (c *RegionCache) GetTiFlashRPCContext(bo *retry.Backoffer, id RegionVerID, 
 		}
 		logutil.Logger(bo.GetCtx()).Info("tcmsdebug GetTiFlashRPCContext cachedRegion is invalid", zap.String("id", id.String()),
 			zap.Bool("isNil", isNil), zap.Bool("syncFlag", syncFlag), zap.Bool("expired", expired))
-		c.mu.RLock()
-		tryGetRegion := c.mu.sorted.getByRegionID(id)
-		c.mu.RUnlock()
-		if isNil {
-			if tryGetRegion != nil {
-				verID := tryGetRegion.VerID()
-				logutil.Logger(bo.GetCtx()).Warn("tcmsdebug get by region id in sorted is not nil", zap.String("id", id.String()),
-					zap.String("find_id", verID.String()))
-			}
-		} else {
-			if tryGetRegion != cachedRegion {
-				logutil.Logger(bo.GetCtx()).Info("tcmsdebug region in sorted and regions is different", zap.Bool("is_valid", tryGetRegion.isValid()))
-			}
-		}
 		return nil, nil
 	}
 
