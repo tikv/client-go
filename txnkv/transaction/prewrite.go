93c2ec8fffedd54b7dc057556ce02c7d0f379c02
you06 <you1474600@gmail.com>
2025-10-28 21:49:47 +0900

    remove IsTempIndexKey which bypass the checks for temp index keys
    Signed-off-by: you06 <you1474600@gmail.com>

--- txnkv/transaction/prewrite.go
+++ txnkv/transaction/prewrite.go
@@ -102,9 +102,8 @@ func (c *twoPhaseCommitter) buildPrewriteRequest(batch batchMutations, txnSize u
 		}
 		if m.IsPessimisticLock(i) {
 			pessimisticActions[i] = kvrpcpb.PrewriteRequest_DO_PESSIMISTIC_CHECK
-		} else if m.NeedConstraintCheckInPrewrite(i) ||
-			(config.NextGen && IsTempIndexKey != nil && !IsTempIndexKey(m.GetKey(i))) {
-			// For next-gen builds, we need to perform constraint checks on all non-temporary index keys.
+		} else if m.NeedConstraintCheckInPrewrite(i) || config.NextGen {
+			// For next-gen builds, we need to perform constraint checks on all non-locked keys.
 			// This is to prevent scenarios where a later lock's start_ts is smaller than the previous write's commit_ts,
 			// which can be problematic for CDC and could potentially break correctness.
 			// see https://github.com/tikv/tikv/issues/11187.
@@ -630,5 +629,3 @@ func (handler *prewrite1BatchReqHandler) handleSingleBatchSucceed(reqBegin time.
 	}
 	return nil
 }
-
-var IsTempIndexKey func([]byte) bool
